b4bb89bc285e790a0e8e196f82232c109e301b6b
Jil Okonma <104331183+CtrlJil@users.noreply.github.com>
2025-01-22 06:41:52 +0100

    models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+from backend.app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    match_id = db.Column(db.Integer, db.ForeignKey('match.id'), nullable=False)
+    qr_code = db.Column(db.String(200), nullable=False)
